1c219b9f1c15a0949f0065546e1d38bc9a85c1dd
sergkryk <sergkryk@ya.ru>
2022-04-02 14:45:41 +0300

    adds assets and styles

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from '@/App.vue'
 // import routes from routes.js //
 import routes from './routes';
 //import css styles //
-import '@/scss/main.scss'
+import '@/sass/main.scss'
 
 const app  = createApp(App)
 app.use(routes);
