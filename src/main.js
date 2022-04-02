49ce23322c8fd16f85e2d6ebcbf7111c840a1835
sergkryk <sergkryk@ya.ru>
2022-04-02 14:20:26 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from '@/App.vue'
+// import routes from routes.js //
+import routes from './routes';
+//import css styles //
+import '@/scss/main.scss'
 
-createApp(App).mount('#app')
+const app  = createApp(App)
+app.use(routes);
+app.mount('#app');
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+// import vue-router functionality //
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+const router = createRouter({
+  history: createWebHistory(), // tell the browser to use built-in mechanism to manage history of your app //
+  routes: [
+
+  ]
+})
+
+export default router;
