19dab6198664eee1f7f4c078548d2723dac2932f
sergkryk <sergkryk@ya.ru>
2022-06-02 07:52:58 +0300

    adds global emitter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from '@/App.vue'
+
+//parent child emitter
+import mitt from 'mitt';
 // import routes from routes.js //
 import routes from '@/routes/routes';
 import store from '@/store/index';
@@ -7,10 +10,16 @@ import store from '@/store/index';
 import '@/sass/main.scss';
 
 import BaseCard from '@/components/ui/BaseCard.vue';
+import BaseModal from '@/components/ui/BaseModal.vue';
 
 const app  = createApp(App)
 
+// to enable parent child comunication (invoke open modal function)
+const emitter = mitt();
+app.config.globalProperties.emitter = emitter;
+// imports global components
 app.component('base-card', BaseCard);
+app.component('base-modal', BaseModal);
 
 app.use(store);
 app.use(routes);
