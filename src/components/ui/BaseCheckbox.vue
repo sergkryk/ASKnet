13e35a69c06ec0ea4ea2918821278069d7d38597
sergkryk <sergkryk@ya.ru>
2022-06-13 14:51:14 +0300

    adds styles to checkbox and radio

--- src/components/ui/BaseCheckbox.vue
+++ src/components/ui/BaseCheckbox.vue
@@ -1,6 +1,7 @@
-.<template>
+.
+<template>
   <div>
-    <input type="checkbox" :name="checkboxName" :id="checkboxId">
+    <input type="checkbox" :name="checkboxName" :id="checkboxId"/>
     <label :for="checkboxId">{{ checkboxLabel }}</label>
   </div>
 </template>
@@ -19,12 +20,95 @@ export default {
     checkboxLabel: {
       type: String,
       required: false,
-      default: '',
+      default: "",
     },
-  }
-}
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+@supports (-webkit-appearance: none) or (-moz-appearance: none) {
+  div {
+    padding: 0.5rem 0;
 
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  input[type="checkbox"] {
+    --size: 1.5rem;
+    --borderColor: #637281;
+    --shadow: #dfe9e4;
+    --fillColor: #ffffff;
+    
+    --checkmarkHeight: 14px;
+    --checkmarkWidth: 7px;
+    --checkmarkColor: #ffffff;
+    --checkmarkBorderWidth: 3px;
+
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+    position: relative;
+
+    width: var(--size);
+    height: var(--size);
+
+    margin-right: 0.5rem;
+
+    outline: none;
+    background-color: var(--fillColor);
+    border: 1px solid var(--borderColor);
+    border-radius: 0.4rem;
+
+    transition: box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out;
+
+    &:after {
+      content: "";
+      position: absolute;
+      top: calc(50% - var(--checkmarkHeight) / 2 - 2px);
+      left: calc(50% - var(--checkmarkWidth) / 2);
+      
+      width: var(--checkmarkWidth);
+      height: var(--checkmarkHeight);
+      
+      border-bottom: var(--checkmarkBorderWidth) solid var(--checkmarkColor);
+      border-right: var(--checkmarkBorderWidth) solid var(--checkmarkColor);
+
+      transform: rotate(45deg);
+      pointer-events: none;
+      transition: opacity 0.3s ease-in-out;
+
+      opacity: 0;
+    }
+    &:checked {
+      --fillColor: #04ab55;
+      --borderColor: #04ab55;
+
+      &::after {
+        opacity: 1;
+      }
+    }
+    &:disabled {
+      --fillColor: #dbe0e4;
+      cursor: not-allowed;
+      pointer-events: none;
+    }
+    &:hover {
+      &:not(:checked) {
+        &:not(:disabled) {
+          box-shadow: 0 0 0 calc(var(--size) / 5) var(--shadow);
+        }
+      }
+    }
+    &:focus {
+      border-color: #04ab55;
+      outline-color: #04ab55;
+    }
+  }
+  label {
+
+  }
+}
 </style>
--- src/components/ui/BaseRadio.vue
+++ src/components/ui/BaseRadio.vue
@@ -25,6 +25,90 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@supports (-webkit-appearance: none) or (-moz-appearance: none) {
+  div {
+    padding: 0.5rem 0;
 
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  input[type="radio"] {
+    --size: 1.5rem;
+    --borderColor: #637281;
+    --shadow: #dfe9e4;
+    --fillColor: #ffffff;
+    
+    --radioSize: calc(var(--size) / 2.5);
+    --radioColor: #ffffff;
+
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+    position: relative;
+
+    width: var(--size);
+    height: var(--size);
+
+    margin-right: 0.5rem;
+
+    outline: none;
+    background-color: var(--fillColor);
+    border: 1px solid var(--borderColor);
+    border-radius: 50%;
+
+    transition: box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out;
+
+    &:after {
+      content: "";
+      
+      position: absolute;
+      top: calc(50% - var(--radioSize) / 2);
+      left: calc(50% - var(--radioSize) / 2);
+      
+      height: var(--radioSize);
+      width: var(--radioSize);
+      
+      opacity: 0;
+      border-radius: 50%;
+      background-color: var(--radioColor);
+
+      transform: scale(0);
+      transform-origin: center;
+      pointer-events: none;
+      transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+
+    }
+    &:checked {
+      --fillColor: #04ab55;
+      --borderColor: #04ab55;
+
+      &::after {
+        transform: scale(1);
+        opacity: 1;
+      }
+    }
+    &:disabled {
+      --fillColor: #dbe0e4;
+      cursor: not-allowed;
+      pointer-events: none;
+    }
+    &:hover {
+      &:not(:checked) {
+        &:not(:disabled) {
+          box-shadow: 0 0 0 calc(var(--size) / 5) var(--shadow);
+        }
+      }
+    }
+    &:focus {
+      border-color: #04ab55;
+      outline-color: #04ab55;
+    }
+  }
+  label {
+
+  }
+}
 </style>
